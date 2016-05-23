b195fa1657c9ae7b7d65f95b96322af88b779143
Justin Mammarella <jmamma@gmail.com>
2016-05-23 17:24:04 +1000

    Changed default image to Cirros Commented out bridge checking and replaced with ping

--- net_stress_test.sh
+++ net_stress_test.sh
@@ -3,25 +3,27 @@
 #Useful for determining inconsitent network behaviour or even lack of metadata
 
 HOST=$1
-CELL=melbourne-qh2-uom
-VMS=12
+CELL=melbourne-qh2
+VMS=10
 N=1
+IMAGE=ed01445a-792e-4a69-8868-9ffa5e7e0c06
     
 counter=0
 
 while [ $counter -lt $N ]; do
-    #Launch VMS
+    echo Launching VMS
     for i in `seq 1 $VMS`; do 
         
         if [ $i -eq $VMS ]; then
-            nova boot --key nectar_jm --image eeedf697-5a41-4d91-a478-01bb21e32cbe --flavor "m1.small" --availability-zone $CELL:$HOST "$HOST $i dpp_net_stress_test" --poll | grep -e "Server building... 100% complete" -e " id " ;     
+            echo Polling last VM..
+            nova boot --key nectar_jm --image $IMAGE --flavor "m1.small" --availability-zone $CELL:$HOST "$HOST $i dpp_net_stress_test" --poll | grep -e "Server building... 100% complete" -e " id " ;     
         else
-            nova boot --key nectar_jm --image eeedf697-5a41-4d91-a478-01bb21e32cbe --flavor "m1.small" --availability-zone $CELL:$HOST "$HOST $i dpp_net_stress_test" | grep -e "Server building... 100% complete" -e " id " ;  
+            nova boot --key nectar_jm --image $IMAGE --flavor "m1.small" --availability-zone $CELL:$HOST "$HOST $i dpp_net_stress_test" | grep -e "Server building... 100% complete" -e " id " ;  
         fi
     done
     #for i in `seq 1 $VMS`; do nova boot --key nectar_jm --image eeedf697-5a41-4d91-a478-01bb21e32cbe --flavor "m1.small" --availability-zone melbourne-qh2:$HOST "$HOST $i dpp_net_stress_test" | grep -e "Server building... 100% complete" -e " id " ; done
     
-    #Wait for VMs to initialise
+    echo Wait for VMs to initialise
 
     while [ $(nova list | grep -e dpp_net_stress_test | grep spawning | wc -l) -gt 0 ]; do
         sleep 1
@@ -29,10 +31,19 @@ while [ $counter -lt $N ]; do
     
     #Sleep for a bit more to give network time to come up
 
-    sleep 10
+    sleep 60
 
     #NMAP scan VMs to determine if network is okay
-    while read -r ip; do nmap $ip | grep "Nmap scan report" -A 1; done < <(nova list | grep "dpp_net_stress_test" | cut -f7 -d'|' | cut -f2 -d'=')
+
+    echo Checking Bridge
+#    for vm in $(nova list | grep "dpp_net_stress_test" | awk '{ print $2 }'); do echo $vm; ./check_bridge.sh $vm; done
+#    nova list
+    echo Running NMAP
+    while read -r ip; do echo checking $ip; nmap $ip | grep "Nmap scan report" -A 1; done < <(nova list | grep "dpp_net_stress_test" | grep ACTIVE | cut -f7 -d'|' | cut -f2 -d'=')
+
+    echo Ping test
+    while read -r ip; do echo checking $ip; ping -i .2 -c 10 $ip | grep loss; done < <(nova list | grep "dpp_net_stress_test" | grep ACTIVE | cut -f7 -d'|' | cut -f2 -d'=')
+    while read -r ip; do echo checking $ip; ping -i .2 -c 10 $ip | grep loss; done < <(nova list | grep "dpp_net_stress_test" | grep ACTIVE | cut -f7 -d'|' | cut -f2 -d'=')
 
     #while read -r ip; do nmap $ip; done < <(nova list | grep "dpp_net_stress_test" | cut -f7 -d'|' | cut -f2 -d'=')
     #Delete VMs
